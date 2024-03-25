5fd729c53504cf73478f42c72e4a63cd6afe5b77
Marian Todorov <meriffa@users.noreply.github.com>
2024-03-25 19:33:54 -0400

    Fixes #32

--- Sources/Devices.Common/AssemblyInfo.cs
+++ Sources/Devices.Common/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("™ 2024 - 2024. Marian Todorov")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyFileVersion("1.2024.03.23")]
+[assembly: AssemblyFileVersion("1.2024.03.25")]
 [assembly: AssemblyInformationalVersion("2024.03")]
