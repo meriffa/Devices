4a3460c36051dbd89fbf9d05aa3faaee52d7ce23
Marian Todorov <meriffa@users.noreply.github.com>
2024-05-05 13:15:14 -0400

    Fixes #50

--- Sources/Devices.Common/AssemblyInfo.cs
+++ Sources/Devices.Common/AssemblyInfo.cs
@@ -2,11 +2,11 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Marian Todorov")]
-[assembly: AssemblyProduct("Marian Todorov IoT Device Management Framework")]
-[assembly: AssemblyCopyright("© 2024 - 2024. Marian Todorov")]
-[assembly: AssemblyTrademark("™ 2024 - 2024. Marian Todorov")]
+[assembly: AssemblyCompany("ByteZoo.com")]
+[assembly: AssemblyProduct("ByteZoo.com Device Management Framework")]
+[assembly: AssemblyCopyright("© 2024 - 2024. ByteZoo.com")]
+[assembly: AssemblyTrademark("™ 2024 - 2024. ByteZoo.com")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyFileVersion("1.2024.05.02")]
+[assembly: AssemblyFileVersion("1.2024.05.05")]
 [assembly: AssemblyInformationalVersion("2024.01")]
