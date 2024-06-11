01d7350764f013145468031d7d2843e99e338234
Marian Todorov <meriffa@users.noreply.github.com>
2024-06-11 09:59:05 -0400

    Fixes #58

--- Sources/Devices.Client.Solutions.Python/CameraPicamera2.py
+++ Sources/Devices.Client.Solutions.Python/CameraPicamera2.py
@@ -14,7 +14,7 @@ class CameraPicamera2:
         self.__camera = picamera2.Picamera2()
         main = {"format": "RGB888", "size": (width, height)}
         controls = {"FrameDurationLimits": (int(1000000 / fps), int(1000000 / fps))}
-        transform = libcamera.Transform(hflip=False, vflip=False)
+        transform = libcamera.Transform(hflip=False, vflip=True)
         configuration = self.__camera.create_preview_configuration(main=main, controls=controls, transform=transform)
         self.__camera.align_configuration(configuration)
         self.__camera.configure(configuration)
